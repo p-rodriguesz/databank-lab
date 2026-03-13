24f380c7936113764c35c3fea68878c3b2a074a4
p-rodriguesz <pedrorodrigues.coelho06@gmail.com>
2026-03-13 10:36:49 -0300

    alterations in create table livro and create table users

--- a1/create_table_user.sql
+++ a1/create_table_user.sql
@@ -5,10 +5,3 @@ username varchar (255) not null,
 email varchar (255) unique,
 data_cadastro date
 );
-CREATE table livro (
-id_livro int auto_increment primary key,
-titulo varchar (255) not null,
-descricao varchar (255) not null,
-data_publicacao year
-);
-
